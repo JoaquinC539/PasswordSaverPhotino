609dfcb5c591393c0f11f526735067a80c61098e
JoaquinC539 <cjoaquinangel@gmail.com>
2025-11-13 18:51:16 -0600

    Scaffolded initial version

--- Classes/GenericJsonObject.cs
+++
@@ -1,7 +0,0 @@
-namespace classes;
-public class GenericJsonObject
-{
-    public bool Error { get; set; }
-    public string? ErrorMessage { get; set; }
-    public bool? Added { get; set; }
-}
--- Classes/TestNes.cs
+++
@@ -1,5 +0,0 @@
-namespace classes;
-public class TestNes
-{
-    public required string TestText { get; set; }
-}
