e9257e83d659058ba0175ff640157a9d2cbbb241
JoaquinC539 <cjoaquinangel@gmail.com>
2025-12-08 12:34:15 -0600

    windows builds

--- winpublishpackaged.ps1
+++ winpublishpackaged.ps1
@@ -1,1 +1,1 @@
-dotnet publish -f net8.0-windows10.0.19041.0 -c Release -p:RuntimeIdentifierOverride=win10-x64 --sc
+dotnet publish -f net8.0-windows10.0.19041.0 -c Release -p:RuntimeIdentifierOverride=win10-x64 -p:GenerateAppxPackageOnbuild=true
