37cc4ee6a1c73ea2466ac2201238523268a2b0a3
JoaquinC539 <cjoaquinangel@gmail.com>
2026-02-10 09:59:48 -0600

    Initial scaffold keeping most of possible, meant to copy and reqrite from Maui-version branch

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+
+Console.WriteLine("Hello World");
+   bool IsLinux = OperatingSystem.IsLinux();
+if (!IsLinux)
+{
+    Console.WriteLine("Not suppoerted platform, only linux");
+    return ;
+} 
+
+try
+{
+    WebKit.Module.Initialize();
+}
+catch (System.Exception)
+{
+    
+    Console.WriteLine("An exception occurred it might have to be because it requires webkit 6");
+    Console.WriteLine("Please run sudo apt install libwebkitgtk-6.0-dev and try again");
+    throw;
+}
+    
+
--- winpublishunpackaged.ps1
+++
@@ -1,1 +0,0 @@
-dotnet publish -c Release -f net8.0-windows10.0.19041.0 -p:RuntimeIdentifierOverride=win10-x64 -p:WindowsPackageType=None --sc -p:IncludeNativeLibrariesForSelfExtract=true
